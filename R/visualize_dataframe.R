4d9a40f09bff46f359cd2210e526d5c9de137e24
JackWilb97 <JackWilburn@tutanota.com>
2018-08-02 15:00:21 -0600

    Update visualize_dataframe to work with long and wide data frames and implemented some better testing

--- R/visualize_dataframe.R
+++ R/visualize_dataframe.R
@@ -1,19 +1,24 @@
 #' Visualize the variables contained in a data frame.
 #'
 #' The visualize_dataframe function helps visualize data frames with a reasonable
-#' number of variables
+#' number of variables. This function is dependent on the number of rows and variables
+#' and will warn you accordingly. The warnings will come for long and wide datasets
+#' as these properties affect performance and the legibility of the plot produced.
 #'
 #' @param dataframe One data frame (as a character string).
 #' @param override Defaults to False. Set to true to override imposed limitations
-#' on the number of variables included in the plot
+#' on the number of variables included in the plot.
 #'
-#' @return A plot that plots each variable against another. This function
-#' is dependent on the number of rows and variables
+#' @return A matrix of plots. Each variable included is graphed against all
+#' other variables and itself.
 #'
 #' @author Sean Raleigh, \email{sraleigh@westminstercollege.edu}
 #' @author Jack Wilburn \email{jackwilburn@tutanota.com}
 #'
-#' @examples visualize_dataframe("CO2")
+#' @examples
+#' visualize_dataframe("CO2")
+#' visualize_dataframe(mtcars, override = TRUE)
+#'
 #'
 #' @importFrom dplyr %>% group_by_ summarise_ ungroup select_
 #' @importFrom tidyr spread_
@@ -22,5 +27,26 @@
 #' @export
 #'
 visualize_dataframe <- function(dataframe, override = FALSE) {
-    ggpairs(dataframe)
+    # If the data frame is long
+    if (nrow(dataframe) > 10000){
+        # Notify about performance issues inherited from ggplot
+        warning("This data set has a lot of rows and you may experience some perfomance
+            issues.")
+    }
+
+    # If manual overide is set to true
+    if (override) {
+        # Plot everything
+        ggpairs(dataframe)
+    } else
+        # if the data frame is wide
+        if (ncol(dataframe) > 10) {
+            # Notify about legibility
+            warning("This data set has a lot of columns so we will only take the first
+                10 so that the plot is still legible.")
+            # Plot the first 10 variables
+            ggpairs(dataframe[, 1:10])
+        } else
+        # Plot everything
+        ggpairs(dataframe)
 }
--- tests/testthat/test_visualize_dataframe.R
+++ tests/testthat/test_visualize_dataframe.R
@@ -1,2 +1,49 @@
 context("visualize_dataframe")
 library(datafinder)
+
+# Make a long data frame
+longdata <- as.data.frame(rbind(EuStockMarkets, EuStockMarkets, EuStockMarkets,
+                                EuStockMarkets, EuStockMarkets, EuStockMarkets))
+
+# Load in all possible permuations
+reasonable <- visualize_dataframe(CO2)
+reasonable_override <- visualize_dataframe(CO2, override = TRUE)
+wide <- visualize_dataframe(mtcars)
+wide_override <- visualize_dataframe(mtcars, override = TRUE)
+long <- visualize_dataframe(longdata)
+long_override <- visualize_dataframe(longdata, override = TRUE)
+
+# Test output classes
+test_that("visualize_dataframe: class gg, ggmatrix", {
+    expect_equal(class(reasonable), c("gg", "ggmatrix"))
+    expect_equal(class(reasonable_override), c("gg", "ggmatrix"))
+    expect_equal(class(wide), c("gg", "ggmatrix"))
+    expect_equal(class(wide_override), c("gg", "ggmatrix"))
+    expect_equal(class(long), c("gg", "ggmatrix"))
+    expect_equal(class(long_override), c("gg", "ggmatrix"))
+})
+
+# Test resonably sized data frames
+test_that("visualize_dataframe: resonably sized", {
+    expect_equal(reasonable$nrow, ncol(CO2))
+})
+
+# Test long data frames
+test_that("visualize_dataframe: wide", {
+    expect_equal(wide$nrow, 10)
+})
+
+# Test wide data frames
+test_that("visualize_dataframe: long", {
+    expect_equal(long$nrow, ncol(longdata))
+})
+
+# Test that override makes a plot the same length as columns
+test_that("visualize_dataframe: override function", {
+    expect_equal(reasonable_override$nrow, ncol(CO2))
+    expect_equal(reasonable_override$ncol, ncol(CO2))
+    expect_equal(wide_override$nrow, ncol(mtcars))
+    expect_equal(wide_override$ncol, ncol(mtcars))
+    expect_equal(long_override$nrow, ncol(longdata))
+    expect_equal(long_override$ncol, ncol(longdata))
+})
