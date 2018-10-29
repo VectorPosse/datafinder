0587c8c7c2b6390842b468fd2160966d5405b465
JackWilb97 <jackwilburn@tutanota.com>
2018-10-29 01:03:35 -0600

    Self contained tests for count_vars and visualize_dataframe. Can't make self contained test for explore_packages. Decreases test time by 25%. Partial fix for issue #11. Might be all we can achieve.

--- tests/testthat/test_count_vars.R
+++ tests/testthat/test_count_vars.R
@@ -1,9 +1,13 @@
 context("count_vars")
 library(datafinder)
 
+longdata <- data.frame(factor = factor(rep(1:10, 1001)),
+                       numeric = rep(1:10, 1001),
+                       logical = rep(c(TRUE, FALSE), 5005))
+
 ## Check if count_vars returns an object of class "data.frame"
 test_that("count_vars: class data.frame", {
-    expect_equal(class(count_vars("CO2")), "data.frame")
+    expect_equal(class(count_vars(longdata)), "data.frame")
 })
 
 ## Check if no input returns error
@@ -14,30 +18,28 @@ test_that("count_vars: count the number of missing dataframe", {
 ## Check if single data object returns data frame of length one with correct
 ## info
 ref_single <- data.frame(
-    Dataframe = "CO2",
-    factor = 2,
-    numeric = 2,
-    ordered = 1,
-    total_vars = 5,
-    sample_size = 84
+    Dataframe = "longdata",
+    factor = 1,
+    integer = 1,
+    logical = 1,
+    total_vars = 3,
+    sample_size = 10010
 )
 test_that("count_vars: count the number of variables in a single dataframe", {
-    expect_equal(count_vars("CO2"), ref_single)
-    expect_equal(count_vars(CO2), count_vars("CO2"))
+    expect_equal(count_vars(longdata), ref_single)
 })
 
 ## Check if multiple data objects returns data frame with correct length and
 ## info
+smallerdata <- longdata[1:100, ]
 ref_multiple <- data.frame(
-        Dataframe = c("CO2", "mtcars"),
-        factor = c(2, 0),
-        numeric = c(2, 11),
-        ordered = c(1, 0),
-        total_vars = c(5, 11),
-        sample_size = c(84, 32)
+    Dataframe = c("longdata", "smallerdata"),
+    factor = c(1, 1),
+    integer = c(1, 1),
+    logical = c(1, 1),
+    total_vars = c(3, 3),
+    sample_size = c(10010, 100)
 )
 test_that("count_vars: count the number of variables in multiple dataframes", {
-              expect_equal(count_vars(c("CO2", "mtcars")), ref_multiple)
-              expect_equal(count_vars(c(CO2, mtcars)),
-                           count_vars(c("CO2", "mtcars")))
+              expect_equal(count_vars(c(longdata, smallerdata)), ref_multiple)
 })
--- tests/testthat/test_visualize_dataframe.R
+++ tests/testthat/test_visualize_dataframe.R
@@ -2,14 +2,15 @@ context("visualize_dataframe")
 library(datafinder)
 
 # Make a long data frame
-longdata <- as.data.frame(rbind(EuStockMarkets, EuStockMarkets, EuStockMarkets,
-                                EuStockMarkets, EuStockMarkets, EuStockMarkets))
+longdata <- data.frame(factor = factor(rep(1:10, 1001)),
+                       numeric = rep(1:10, 1001),
+                       logical = rep(c(TRUE, FALSE), 5005))
 
 # Load in all possible permuations
-reasonable <- visualize_dataframe(CO2)
-reasonable_override <- visualize_dataframe(CO2, override = TRUE)
-expect_warning(wide <- visualize_dataframe(mtcars))
-wide_override <- visualize_dataframe(mtcars, override = TRUE)
+reasonable <- visualize_dataframe(longdata[1:1000, ])
+reasonable_override <- visualize_dataframe(longdata[1:1000, ], override = TRUE)
+expect_warning(wide <- visualize_dataframe(longdata[1:1000 , c(1:3, 1:3, 1:3, 1:3)]))
+wide_override <- visualize_dataframe(longdata[1:1000 , c(1:3, 1:3, 1:3, 1:3)], override = TRUE)
 expect_warning(long <- visualize_dataframe(longdata))
 expect_warning(long_override <- visualize_dataframe(longdata, override = TRUE))
 
@@ -21,7 +22,7 @@ test_that("visualize_dataframe: pass dataframe as string or not", {
 
 # Test that wide and long dataframes giv warnings
 test_that("visualize_dataframe: warnings work as expected", {
-    expect_warning(wide <- visualize_dataframe(mtcars))
+    expect_warning(wide <- visualize_dataframe(longdata[1:1000 , c(1:3, 1:3, 1:3, 1:3)]))
     expect_warning(long <- visualize_dataframe(longdata))
     expect_warning(long_override <- visualize_dataframe(longdata,
                                                         override = TRUE))
@@ -39,7 +40,7 @@ test_that("visualize_dataframe: class gg, ggmatrix", {
 
 # Test resonably sized data frames
 test_that("visualize_dataframe: resonably sized works", {
-    expect_equal(reasonable$nrow, ncol(CO2))
+    expect_equal(reasonable$nrow, ncol(longdata[1:1000, ]))
 })
 
 # Test long data frames
@@ -54,10 +55,10 @@ test_that("visualize_dataframe: long no variables are lost", {
 
 # Test that override makes a plot the same length as columns
 test_that("visualize_dataframe: override function plots correctly", {
-    expect_equal(reasonable_override$nrow, ncol(CO2))
-    expect_equal(reasonable_override$ncol, ncol(CO2))
-    expect_equal(wide_override$nrow, ncol(mtcars))
-    expect_equal(wide_override$ncol, ncol(mtcars))
+    expect_equal(reasonable_override$nrow, ncol(longdata[1:1000, ]))
+    expect_equal(reasonable_override$ncol, ncol(longdata[1:1000, ]))
+    expect_equal(wide_override$nrow, ncol(longdata[1:1000 , c(1:3, 1:3, 1:3, 1:3)]))
+    expect_equal(wide_override$ncol, ncol(longdata[1:1000 , c(1:3, 1:3, 1:3, 1:3)]))
     expect_equal(long_override$nrow, ncol(longdata))
     expect_equal(long_override$ncol, ncol(longdata))
 })
